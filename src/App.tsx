a683d1ab03d2532ccc52a135fbc2402d40345369
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-07-24 15:08:08 -0700

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import Home from './components/Home';
 import ExamplePage from './components/ExamplePage';
 import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 
-import { AppBar, Tabs, Tab, ThemeProvider } from '@material-ui/core';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#6C4087',
-      // dark: will be calculated from palette.primary.main,
-    },
-    secondary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#C71C5B',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    },
-    text: {
-      //primary: '',
-      //secondary: ''
-    },
-  },
-  typography: {
-    h1: {
-      fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
-    },
-    h2: {
-      fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
-    },
-    h3: {
-      fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
-    },
-    h4: {
-      fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
-    },
-  },
-});
+import theme from './theme';
 
 const App: React.FC = () => {
   return (
@@ -55,12 +23,7 @@ const App: React.FC = () => {
         </Helmet>
 
         <Router>
-          <AppBar position="static" color="transparent">
-            <Tabs value={false} variant="scrollable" scrollButtons="auto">
-              <Tab label="Home" component={Link} to="/" />
-              <Tab label="Other Example Page" component={Link} to="/example" />
-            </Tabs>
-          </AppBar>
+          <NavBar />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/example" exact component={ExamplePage} />
