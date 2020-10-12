c0a0f1f6aee5a98e32fe0ef5e8b5f791c8fe6db8
Aaron Pradhan <12a.pradhan@gmail.com>
2020-10-12 10:55:12 -0400

    Create login page

---
+++ src/containers/login/Login.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './login.less';
+import { Button, Checkbox, Form, Input, Typography } from 'antd';
+import { login } from '../../auth/authAPI';
+import { Link } from 'react-router-dom';
+const { Title, Paragraph } = Typography;
+
+const Login: React.FC = () => {
+  const onFinish = (values: any) => {
+    login({ email: values.username, password: values.password })
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="Description goes here." />
+      </Helmet>
+      <div className="content-container">
+        {/*
+          Place relevant components in here
+        */}
+        <Title>Login</Title>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Paragraph>
+            Need an account? Sign up <Link to="/signup" component={Typography.Link}>here</Link>!
+          </Paragraph>
+
+          <Paragraph>
+            Forgot your password? Click <Link to="/" component={Typography.Link}>here</Link> to reset it.
+          </Paragraph>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
