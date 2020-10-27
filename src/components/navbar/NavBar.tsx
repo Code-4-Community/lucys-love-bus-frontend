3488cf7712a8df3cbacd9098c8ca975aa49dc69a
Anirudh Singh <singh.anir@northeastern.edu>
2020-10-26 21:10:14 -0400

    remove unwanted code

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Menu, Dropdown, Image, Typography, Row, Col, Button } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
@@ -7,39 +7,16 @@ import './Navbar.less'
 
 const { Text } = Typography;
 
-
-const NavBar: React.FC = (props) => {
+const NavBar: React.FC = () => {
   const history = useHistory();
-  const location = useLocation();
-  const path: string = location.pathname;
-
-  function pathnameToKey(pathname: string) {
-    switch (pathname) {
-      case '/':
-        return '1';
-      case '/block-template':
-        return '2';
-      case '/grid-template':
-        return '3';
-      case '/change-primary-email':
-        return '4';
-      case '/account-details':
-        return '5';
-      case '/change-password':
-        return '6';
-      case '/deactivate-account':
-        return '7';
-      default:
-        return '1';
-    }
-  }
+  const [authenticated, setAuthenticated] = useState(true);
 
   const userMenu = (
     <Menu>
-      <Menu.Item key="4">Change Primary Account Email</Menu.Item>
-      <Menu.Item key="5">Account Details</Menu.Item>
-      <Menu.Item key="6">Change Password</Menu.Item>
-      <Menu.Item key="7">Deactivate Account</Menu.Item>
+      <Menu.Item>Change Primary Account Email</Menu.Item>
+      <Menu.Item>Account Details</Menu.Item>
+      <Menu.Item>Change Password</Menu.Item>
+      <Menu.Item>Deactivate Account</Menu.Item>
     </Menu>
   );
 
@@ -48,28 +25,23 @@ const NavBar: React.FC = (props) => {
       <Col flex={1}>
         <Link to="/">
           <Image 
-            width={90} 
-            height={80} 
+            className="llbLogo" 
             src={"https://lucys-love-bus-public.s3.us-east-2.amazonaws.com/LLB_2019_Sq_rgb+1.png"} 
-            alt={"LLB Logo"} 
-            style={{float: "right"}}
+            alt={"LLB Logo"}
           />
         </Link>
       </Col>
+
       <Col flex={1}>
-        <Text 
-          strong 
-          style={{fontSize: "24px"}}
-          >
+        <Text className="llbText" strong>
           Lucy's Love Bus
         </Text>
         <br/>
-        <Text 
-          strong 
-        >
+        <Text strong>
           Event Registration
         </Text>
       </Col>
+
       <Col flex={3}>
         <Button 
           type="link"
@@ -96,35 +68,40 @@ const NavBar: React.FC = (props) => {
           My Events
         </Button>
       </Col>
-      {/* <Col flex={3}>
-      <Dropdown overlay={userMenu}>
-        <Button className="userDropdown">
-          <UserOutlined /> John Smith <DownOutlined />
-        </Button>
-      </Dropdown>
-      </Col> */}
-      <Col flex={3}>
-        <div className="loginAndSignupBtns">
-          <Button 
-            type="link" 
-            className="loginBtn"
-            onClick={() => {
-              history.push('/login');
-            }}
-          >
-            Login
-          </Button>
-          <Button 
-            className="signUpBtn"
-            onClick={() => {
-              history.push('/signup');
-            }}
-          >
-            Sign Up
-          </Button>
-        </div>
 
-      </Col>
+      <div>
+        {authenticated ? 
+          <Col flex={3}>
+            <Dropdown overlay={userMenu}>
+              <Button className="userDropdown">
+                <UserOutlined /> John Smith <DownOutlined />
+              </Button>
+            </Dropdown>
+          </Col>
+        : <Col flex={3}>
+            <div className="loginAndSignupBtns">
+              <Button 
+                type="link" 
+                className="loginBtn"
+                onClick={() => {
+                  history.push('/login');
+                }}
+              >
+                Login
+              </Button>
+              <Button 
+                className="signUpBtn"
+                onClick={() => {
+                  history.push('/signup');
+                }}
+              >
+                Sign Up
+              </Button>
+            </div>
+          </Col>
+        }
+      </div>
+
     </Row>
   );
 };
