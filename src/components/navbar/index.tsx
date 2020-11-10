54fd0edd00ed7ab7b943a41740d014a47a175bda
Anirudh Singh <singh.anir@northeastern.edu>
2020-11-10 11:17:32 -0500

    Added styled components

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,14 +3,28 @@ import { useHistory } from 'react-router-dom';
 import { Menu, Dropdown, Typography, Row, Col, Button } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import './navbar.less';
 
 const { Text } = Typography;
+
+// Custom styling for the navbar links - Home, Upcoming Events, My Events
+const StyledButton = styled(Button)`
+  color: black;
+  margin-left: 7em;
+`
+
+// Custom styling for the navbar row
+const StyledRow = styled(Row)`
+  background-color: white;
+  padding: 1em 0;
+`
 
 const NavBar: React.FC = () => {
   const history = useHistory();
   const authenticated = false;
 
+  // Dropdown menu options for the logged in
   const userMenu = (
     <Menu>
       <Menu.Item>Change Primary Account Email</Menu.Item>
@@ -21,7 +35,7 @@ const NavBar: React.FC = () => {
   );
 
   return (
-    <Row align="middle">
+    <StyledRow align="middle">
       <Col flex={1}>
         <Link
           to="/"
@@ -31,8 +45,8 @@ const NavBar: React.FC = () => {
         >
           <img
             className="llbLogo"
-            src="https://lucys-love-bus-public.s3.us-east-2.amazonaws.com/LLB_2019_Sq_rgb+1.png"
-            alt="LLB Logo"
+            src='https://lucys-love-bus-public.s3.us-east-2.amazonaws.com/LLB_2019_Sq_rgb+1.png'
+            alt='LLB Logo'
           />
         </Link>
       </Col>
@@ -47,36 +61,35 @@ const NavBar: React.FC = () => {
           <Text className="llbText" strong>
             Lucy's Love Bus
           </Text>
-          <br />
           <Text strong>Event Registration</Text>
         </Link>
       </Col>
 
       <Col flex={3}>
-        <Button
+        <StyledButton
           type="link"
           onClick={() => {
             history.push('/');
           }}
         >
           Home
-        </Button>
-        <Button
+        </StyledButton>
+        <StyledButton
           type="link"
           onClick={() => {
             history.push('/block-template');
           }}
         >
           Upcoming Events
-        </Button>
-        <Button
+        </StyledButton>
+        <StyledButton
           type="link"
           onClick={() => {
             history.push('/grid-template');
           }}
         >
           My Events
-        </Button>
+        </StyledButton>
       </Col>
 
       <div>
@@ -111,7 +124,8 @@ const NavBar: React.FC = () => {
           </Col>
         )}
       </div>
-    </Row>
+
+    </StyledRow>
   );
 };
 
