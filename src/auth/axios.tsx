f34006b98e17059f9415a2afcb22c9514c3c7b46
Jack Blanc <jblanc222@gmail.com>
2020-11-15 18:02:38 -0500

    remove unused axios func to improve test coverage

--- src/auth/axios.tsx
+++ src/auth/axios.tsx
@@ -1,6 +1,5 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import tokenService from './token';
-// import { refresh, logout } from './authClient';
 
 const Instance: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_DOMAIN,
@@ -20,35 +19,33 @@ export const requestInterceptor: (
 
 Instance.interceptors.request.use(requestInterceptor);
 
-const INVALID_ACCESS_TOKEN = 'Given access token is expired or invalid';
-
-const responseErrorInterceptor: (error: AxiosError) => void = (error) => {
-  // const originalRequest = error.config;
-  if (
-    error.code === '401' &&
-    error.message === INVALID_ACCESS_TOKEN &&
-    tokenService.isRefreshTokenValid()
-  ) {
-    // return refresh().then(() => {
-    //   Instance.defaults.headers[
-    //     'X-Access-Token'
-    //   ] = tokenService.getAccessToken();
-    //   return Instance(originalRequest);
-    // });
-  }
-  if (
-    error.code === '401' &&
-    error.message === INVALID_ACCESS_TOKEN &&
-    !tokenService.isRefreshTokenValid()
-  ) {
-    // logout();
-  }
-  return Promise.reject(error);
-};
+// const responseErrorInterceptor: (error: AxiosError) => void = (error) => {
+//   // const originalRequest = error.config;
+//   if (
+//     error.code === '401' &&
+//     error.message === INVALID_ACCESS_TOKEN &&
+//     tokenService.isRefreshTokenValid()
+//   ) {
+//     // return refresh().then(() => {
+//     //   Instance.defaults.headers[
+//     //     'X-Access-Token'
+//     //   ] = tokenService.getAccessToken();
+//     //   return Instance(originalRequest);
+//     // });
+//   }
+//   if (
+//     error.code === '401' &&
+//     error.message === INVALID_ACCESS_TOKEN &&
+//     !tokenService.isRefreshTokenValid()
+//   ) {
+//     // logout();
+//   }
+//   return Promise.reject(error);
+// };
 
 Instance.interceptors.response.use(
   (response) => response,
-  responseErrorInterceptor,
+  (error) => error,
 );
 
 export default Instance;
