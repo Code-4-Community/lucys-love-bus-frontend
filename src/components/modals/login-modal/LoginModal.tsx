aee39b24bc7e77954f4a9ea2f5b7681479995a35
Varun Thakkar <varunthakkar2013@gmail.com>
2020-12-06 17:48:02 -0800

    updated modal to have all content defined in one class

--- src/components/modals/login-modal/LoginModal.tsx
+++ src/components/modals/login-modal/LoginModal.tsx
@@ -1,62 +1,157 @@
 import React, { useState } from 'react';
-import { Modal } from 'antd';
+import { Button, Form, Input, Modal, Typography } from 'antd';
 import styled from 'styled-components';
 import './login-modal.less';
-import LoginContent from './LoginContent';
-import ForgotPasswordContent from './ForgotPasswordContent';
-import PasswordResetContent from './PasswordResetContent';
+import { login } from '../../../auth/ducks/thunks';
+import { useDispatch } from 'react-redux';
+import Text from 'antd/es/typography/Text';
+import { Link } from 'react-router-dom';
 
 interface LoginModalProps {
   showLoginModal: boolean;
   onCloseLoginModal: () => void;
 }
 
+enum ModalContent {
+  LoginContent,
+  ForgotPassword,
+  ResetPassword,
+}
+
 const StyledModal = styled(Modal)`
-  border-radius: 33px;
   horiz-align: center;
-  .ant-modal-content {
-    height: 534px;
-    border-radius: 33px;
-  }
 `;
 
-const getModalContent = (
-  currentPage: string,
-  switchToForgotPasswordPage: () => void,
-  switchToPasswordResetPage: () => void,
-) => {
-  switch (currentPage) {
-    case 'login':
-      return (
-        <LoginContent switchToForgotPasswordPage={switchToForgotPasswordPage} />
-      );
-    case 'forgotPassword':
-      return (
-        <ForgotPasswordContent
-          switchToConfirmationPage={switchToPasswordResetPage}
-        />
-      );
-    case 'passwordReset':
-      return <PasswordResetContent />;
-  }
-};
+const ContentDiv = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+`;
 
-const LoginModal = (props: LoginModalProps) => {
-  const [currentPage, setPage] = useState('login');
+const ForgotPasswordLinkButton = styled(Typography.Link)`
+  color: black;
+  white-space: nowrap;
+  background-color: transparent;
+  border: transparent;
+  padding: 0px 0px 0px 0px;
+`;
+
+const ForgotPasswordText = styled(Text)`
+  color: #000000;
+`;
+
+const EmailField = styled(Input)``;
+
+const LoginModal: React.FC<LoginModalProps> = (props: LoginModalProps) => {
+  const [currentPage, setPage] = useState<ModalContent>(
+    ModalContent.LoginContent,
+  );
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const dispatch = useDispatch();
 
   const setToForgotPasswordPage = () => {
-    setPage('forgotPassword');
+    setPage(ModalContent.ForgotPassword);
   };
 
   const setToPasswordResetPage = () => {
-    setPage('passwordReset');
+    setPage(ModalContent.ResetPassword);
+  };
+
+  const getModalContent = (
+      currentPage: ModalContent,
+      switchToForgotPasswordPage: () => void,
+      switchToPasswordResetPage: () => void,
+  ) => {
+    switch (currentPage) {
+      case ModalContent.LoginContent:
+        return (
+            <ContentDiv>
+              <EmailField placeholder="Email"/>
+              <Input.Password placeholder="Password" />
+              <ForgotPasswordText className="forgot-password-text">
+                Forgot password? Click{' '}
+                <ForgotPasswordLinkButton onClick={switchToForgotPasswordPage}>
+                  here
+                </ForgotPasswordLinkButton>
+              </ForgotPasswordText>
+              <Text className="sign-up-text">
+                Don’t have an account? Sign up{' '}
+                <Link to="/signup" component={Typography.Link}>
+                  here
+                </Link>
+              </Text>
+            </ContentDiv>
+        );
+      case ModalContent.ForgotPassword:
+        return (
+            <div className="content">
+              <Text>
+                Enter email address associated with account
+              </Text>
+              <Text>Email</Text>
+              <Input placeholder="Email" />
+            </div>
+        );
+      case ModalContent.ResetPassword:
+        return (
+            <div className="content">
+              <Text className="password-reset-text">
+                Keep an eye on your inbox (and check your spam folder as well). If
+                you still haven’t received an email,{' '}
+                <Button className="resend-email-page-link" type="link">
+                  click here
+                </Button>{' '}
+                to resend it
+              </Text>
+            </div>
+        );
+    }
+  };
+
+  const getTitle = (currentPage: ModalContent): string => {
+    switch (currentPage) {
+      case ModalContent.LoginContent:
+        return 'Log in to your account';
+      case ModalContent.ForgotPassword:
+        return 'Reset your password';
+      case ModalContent.ResetPassword:
+        return 'Your password reset link is on the way!';
+    }
+  };
+
+  const handleOk = (): void => {
+    switch (currentPage) {
+      case ModalContent.LoginContent:
+        dispatch(login({ email, password }));
+        break;
+      case ModalContent.ForgotPassword:
+        setPage(ModalContent.ResetPassword);
+        break;
+      case ModalContent.ResetPassword:
+        props.onCloseLoginModal();
+        break;
+    }
+  };
+
+  const getOkText = (): string => {
+    switch (currentPage) {
+      case ModalContent.LoginContent:
+        return 'Sign In';
+      case ModalContent.ForgotPassword:
+        return 'Send Link';
+      case ModalContent.ResetPassword:
+        return '';
+    }
   };
 
   return (
     <div className="modal">
       <StyledModal
         visible={props.showLoginModal}
-        footer={null}
+        title={getTitle(currentPage)}
+        onOk={handleOk}
+        okText={getOkText()}
         onCancel={props.onCloseLoginModal}
         width={'625px'}
       >
