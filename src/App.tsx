d5378bdc5881e6881f49d7bb3145da19386ef1fe
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-12-28 18:27:09 -0500

    deleted other signup css

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,9 @@ import Home from './containers/home/Home';
 import Settings from './containers/settings/Settings';
 import BlockTemplate from './containers/template-1-col-block/Template';
 import GridTemplate from './containers/template-24-col-grid/Template';
-import SignupConfirmationPF from './containers/signup-confirmation/SignupConfirmationPF';
-import SignupConfirmationGM from './containers/signup-confirmation/SignupConfirmationGM';
 import NotFound from './containers/not-found/NotFound';
 import NavBar from './components/navbar';
 import Footer from './components/Footer';
-import Signup_PF_P1 from './containers/signup-pf-p1/Signup_PF_P1';
-import Signup_PF_P2 from './containers/signup-pf-p2/Signup_PF_P2';
 import UpcomingEvents from './containers/upcoming-events/UpcomingEvents';
 import SignupFlow from './containers/signupFlow';
 import { Layout } from 'antd';
@@ -43,18 +39,6 @@ const App: React.FC = () => {
               <Route path="/block-template" exact component={BlockTemplate} />
               <Route path="/grid-template" exact component={GridTemplate} />
               <Route path="/signup" component={SignupFlow} />
-              <Route
-                path="/signup-confirmation-gm"
-                exact
-                component={SignupConfirmationGM}
-              />
-              <Route
-                path="/signup-confirmation-pf"
-                exact
-                component={SignupConfirmationPF}
-              />
-              <Route path="/signup-pf-p1" exact component={Signup_PF_P1} />
-              <Route path="/signup-pf-p2" exact component={Signup_PF_P2} />
               <Route path="/upcoming-events" exact component={UpcomingEvents} />
               <Route path="/settings" exact component={Settings} />
               <Route path="*" exact component={NotFound} />
