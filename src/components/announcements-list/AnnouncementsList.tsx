57e58105221752d590df9c631365f726494d19bb
Anirudh Singh <singh.anir@northeastern.edu>
2021-02-06 02:10:35 -0500

    hooked up GET request
    hooked up the newly exposed public route to get all announcement data, populating each card with the appropriate fields. we will need to add a src field in the db

--- src/components/announcements-list/AnnouncementsList.tsx
+++ src/components/announcements-list/AnnouncementsList.tsx
@@ -1,11 +1,11 @@
 import { Row } from 'antd';
 import React from 'react';
 import { AnnouncementCard } from '../AnnouncementCard';
-import { AnnouncementCardProps } from '../AnnouncementCard';
 import styled from 'styled-components';
 import axios, { AxiosInstance } from 'axios';
 
 const COLUMNS_PER_ROW = 3
+const NO_ANNOUNCEMENTS_MESSAGE = "There are currently no announcements!"
 const ANNOUNCEMENTS = '/api/v1/announcements/'
 const AuthAxiosInstance: AxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_DOMAIN,
@@ -14,84 +14,46 @@ const AuthAxiosInstance: AxiosInstance = axios.create({
         'Content-Type': 'application/json',
     },
 });
-const getAllAnnouncements: () => void = () => {
-    AuthAxiosInstance.get(ANNOUNCEMENTS).then(
-        (response) => console.log(response.data)
-    );
-}
     
 const AnnouncementRow = styled(Row)`
     display: grid;
-    grid-auto-flow: column;
+    grid-template-columns: 1fr 1fr 1fr;
     margin-bottom: 20px;
 `
 
 export const AnnouncementsList: React.FC = () => {
-    getAllAnnouncements();
-    // mock data to use for now
-    const announcement1: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/12/12'),
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ',
-        src:
-            'https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg',
-    };
-    const announcement2: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/11/28'),
-        description:
-            'Find some time for self-care and join yoga teacher Sarah Oleson for a peaceful and rejuvenating virtual restorative yoga session! Find a comfortable spot and grab a mat and a blanket and/or bolster. Open to all abilities. ',
-        src:
-            'https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg',
-    };
-    const announcement3: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/11/18'),
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    };
-    const announcement4: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/11/18'),
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        src:
-            'https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg',
-    };
-    const announcement5: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/11/18'),
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    };
-    const announcement6: AnnouncementCardProps = {
-        title: 'VIRTUAL Slow Flow Restorative Yoga',
-        date: new Date('2020/11/28'),
-        description:
-            'Find some time for self-care and join yoga teacher Sarah Oleson for a peaceful and rejuvenating virtual restorative yoga session! Find a comfortable spot and grab a mat and a blanket and/or bolster. Open to all abilities. ',
-        src:
-            'https://facts.net/wp-content/uploads/2020/07/monarch-butterfly-facts.jpg',
-    };
+    const [announcements, setAnnouncements] = React.useState<Array<any>>([]);
 
-    const announcements: AnnouncementCardProps[] = [announcement1, announcement2, announcement3, announcement4, announcement5, announcement6];
+    React.useEffect(() => {
+        (async function () {
+            let announcementsData = await AuthAxiosInstance.get(ANNOUNCEMENTS)
+                .then(response => response.data)
+                .then(data => { return data.announcements as any[] })
+                .catch(() => { return [] as any[] });
+            setAnnouncements(announcementsData);
+        })()
+    }, []);
 
-    let rows = [...Array(Math.ceil(announcements.length / COLUMNS_PER_ROW))];
+    let numRows = Math.ceil(announcements.length / COLUMNS_PER_ROW);
+    let rows = [...Array(numRows)];
     let announcementRows = rows.map((row, i) => announcements.slice(i * COLUMNS_PER_ROW, i * COLUMNS_PER_ROW + COLUMNS_PER_ROW));
     return (
-        <div className="cards">
+        <div>
             {
-                announcementRows.map((row, i) => {
-                    return (
-                        <AnnouncementRow>
-                            {row.map((announcement, i) => {
-                                return (
-                                    <AnnouncementCard {...announcement} key={i} />
-                                )
-                            })}
-                        </AnnouncementRow>
-                    )
-                })
+                rows.length > 1 ? (
+                    announcementRows.map((row, i) => {
+                        return (
+                            <AnnouncementRow>
+                                {row.map((announcement, i) => {
+                                    return (
+                                        announcement.src ? <AnnouncementCard {...{ src: announcement.src, title: announcement.title, date: new Date(announcement.created), description: announcement.description }} key={i} />
+                                            : <AnnouncementCard {...{ title: announcement.title, date: new Date(announcement.created), description: announcement.description }} key={i} />
+                                    )
+                                })}
+                            </AnnouncementRow>
+                        )
+                    })
+                ) : NO_ANNOUNCEMENTS_MESSAGE
             }
         </div>
     );
