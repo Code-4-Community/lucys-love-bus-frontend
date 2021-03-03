90b0f3d2c129f3e3f29bd50a57f9397ecca71079
Varun Thakkar <varunthakkar2013@gmail.com>
2021-03-03 17:28:48 -0500

    added pagination

--- src/components/announcements-list/AnnouncementsList.tsx
+++ src/components/announcements-list/AnnouncementsList.tsx
@@ -24,25 +24,70 @@ const NoAnnouncementsText = styled(NoAnnouncementsSubText)`
   font-weight: 800;
 `;
 
+const PageNumbersWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const PageNumber = styled.li`
+  font-size: 30px;
+  list-style-type: none;
+  margin: 1rem;
+  cursor: pointer;
+`;
+
 export interface AnnouncementsListProps {
   announcements: AnnouncementProps[];
 }
 
 const AnnouncementsList: React.FC<AnnouncementsListProps> = (props) => {
-  const [numRows, setNumRows] = useState(0);
+  const [numRows, setNumRows] = useState(2);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  useEffect(() => {
-    const rows = Math.ceil(props.announcements.length / COLUMNS_PER_ROW);
-    setNumRows(rows);
-  }, [props.announcements]);
+  const handlePageClick = (event: any) => {
+    setCurrentPage(event.target.id);
+  };
+
+  const rowArray = Array(numRows);
+  const lastRowOfPage = (numRows * currentPage) - 1;
+  const firstRowOfPage = lastRowOfPage - (numRows - 1);
+
+  let index = 0;
+  for (let i = firstRowOfPage; i <= lastRowOfPage; i++) {
+    rowArray[index] = i;
+    index++;
+  }
+
+  console.log(props.announcements.slice(
+    (1 * COLUMNS_PER_ROW) * currentPage,
+    (1 * COLUMNS_PER_ROW + COLUMNS_PER_ROW) * currentPage,
+  ));
 
   const getAnnouncementRows = () =>
-    [...Array(numRows)].map((row, i) =>
+    [rowArray].map((row, i) =>
       props.announcements.slice(
-        i * COLUMNS_PER_ROW,
-        i * COLUMNS_PER_ROW + COLUMNS_PER_ROW,
-      ),
+        (i * COLUMNS_PER_ROW) * currentPage,
+        (i * COLUMNS_PER_ROW + COLUMNS_PER_ROW) * currentPage,
+      )
     );
+
+
+
+  console.log(getAnnouncementRows())
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(props.announcements.length / (numRows * COLUMNS_PER_ROW)); i++) {
+    pageNumbers.push(i);
+  }
+
+  const renderPageNumbers = pageNumbers.map((number) => {
+    return (
+      <PageNumber key={number} id={number.toString()} onClick={handlePageClick}>
+        {number}
+      </PageNumber>
+    );
+  });
 
   return (
     <>
@@ -64,6 +109,9 @@ const AnnouncementsList: React.FC<AnnouncementsListProps> = (props) => {
           </NoAnnouncementsSubText>
         </div>
       )}
+      <PageNumbersWrapper>
+        {renderPageNumbers}
+      </PageNumbersWrapper>
     </>
   );
 };
