e6403d2b47e23a5dacda646fa0e4e9286bc0e492
onb6 <blierolivia@gmail.com>
2021-03-07 14:29:47 -0500

    edited rules for antd password input to check length of password

--- src/containers/forgotPasswordReset/index.tsx
+++ src/containers/forgotPasswordReset/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
-import { Button, Form, Input, Typography } from 'antd';
+import { Button, Form, Input, Typography, Alert } from 'antd';
 import authClient from '../../auth/authClient';
 
 const { Title } = Typography;
@@ -14,8 +14,37 @@ interface NewPasswords {
 const ForgotPasswordReset: React.FC = () => {
   const { key } = useParams();
 
+  const [state, setState] = useState<NewPasswords>({
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [message, setMessage] = useState<string>('')
+
+  const updatePassword = (e: React.ChangeEvent<HTMLInputElement> ) => {
+    setState({password: e.target.value, confirmPassword: state.confirmPassword })
+    checkPasswords();
+  }
+
+  const updateConfirmPassword = (e: React.ChangeEvent<HTMLInputElement> ) => {
+    setState({password: state.password, confirmPassword: e.target.value })
+    checkPasswords();
+  }
+
+  const checkPasswords = () => {
+    if (state.password !== state.confirmPassword) {
+      setMessage("Passwords don't match");
+    } else {
+      setMessage('');
+    }
+  }
+
+  const differentPasswords: () => boolean = () =>
+    state.password !== state.confirmPassword;
+
   const onFinish = (values: NewPasswords) => {
-    if (values.password.length < 8) {
+    if (message !== '') {
+
       alert('New password is too weak. Must be at least 8 characters long.');
     } else if (values.password !== values.confirmPassword) {
       alert("Passwords don't match");
@@ -44,9 +73,10 @@ const ForgotPasswordReset: React.FC = () => {
             name="password"
             rules={[
               { required: true, message: 'Please enter your new password!' },
+              { min: 8, message: 'New password is too weak. Must be at least 8 characters long.' }
             ]}
           >
-            <Input.Password />
+            <Input.Password onChange={updatePassword}/>
           </Form.Item>
 
           <Form.Item
@@ -54,9 +84,10 @@ const ForgotPasswordReset: React.FC = () => {
             name="confirmPassword"
             rules={[
               { required: true, message: 'Please confirm your new password!' },
+              { required: differentPasswords(), message: "Passwords don't match"}
             ]}
           >
-            <Input.Password />
+            <Input.Password onChange={updateConfirmPassword}/>
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
