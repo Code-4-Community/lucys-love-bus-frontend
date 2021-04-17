0d5048e9ebfcc095bbf70b91efee310abf2fa5d3
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-04-17 17:48:34 -0400

    remove redundant redux

--- src/containers/userDirectory/ducks/actions.ts
+++
@@ -1,9 +0,0 @@
-import { genericAsyncActions } from '../../../utils/asyncRequest';
-import { Registration } from './types';
-
-export const eventRegistrations = genericAsyncActions<Registration[], any>();
-
-export type EventRegistrationsActions =
-  | ReturnType<typeof eventRegistrations.loading>
-  | ReturnType<typeof eventRegistrations.loaded>
-  | ReturnType<typeof eventRegistrations.failed>;
