6486c042079960ec1a68f5eedb0d589f5cbacca5
onb6 <blierolivia@gmail.com>
2021-04-19 20:46:18 -0400

    fixed routing to edit form

--- src/components/event-listing/EventListing.tsx
+++ src/components/event-listing/EventListing.tsx
@@ -116,7 +116,7 @@ const EventListing: React.FC<EventListingProps> = ({
           {admin ? (
             <AdminButtonWrapper>
               <GreenButton to={BASE_EVENTS_ROUTE + id}>Learn More</GreenButton>
-              <GreenButton to={BASE_EVENTS_ROUTE + id}>Edit</GreenButton>
+              <GreenButton to={BASE_EVENTS_ROUTE + id + '/edit'}>Edit</GreenButton>
               <GreenButton to={BASE_EVENTS_ROUTE + id}>
                 Make Announcement
               </GreenButton>
