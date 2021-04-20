6bd08dc0cca6571036f596b1667551899949af2e
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-04-19 21:11:16 -0400

    init

---
+++ src/containers/createAnnouncement/index.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { connect, useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Routes } from '../../App';
+import { signup } from '../../auth/ducks/thunks';
+import { UserAuthenticationReducerState } from '../../auth/ducks/types';
+import { ContentContainer } from '../../components';
+import SignupForm from '../../components/SignupForm';
+import { C4CState } from '../../store';
+import { asyncRequestIsComplete } from '../../utils/asyncRequest';
+import { convertToYearMonthDateString } from '../../utils/dateUtils';
+import { encodeProfileFieldFile } from '../../utils/fileEncoding';
+import {
+  participatingFamilySearchQuery,
+  participatingFamilySearchQueryFlag,
+} from '../../utils/signupFlow';
+import { SignupData } from './ducks/types';
+interface SignupFormContainerProps {
+  readonly tokens: UserAuthenticationReducerState['tokens'];
+}
+
+const SignupFormContainer: React.FC<SignupFormContainerProps> = ({
+  tokens,
+}) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const registeringAsParticipatingFamily = new URLSearchParams(
+    location.search,
+  ).has(participatingFamilySearchQueryFlag);
+
+  if (asyncRequestIsComplete(tokens)) {
+    if (registeringAsParticipatingFamily) {
+      history.push({
+        pathname: Routes.SET_CONTACTS,
+        search: participatingFamilySearchQuery,
+      });
+    } else {
+      history.push(Routes.SIGNUP_CONFIRMATION);
+    }
+  }
+
+  const onFinish = async (data: SignupData) => {
+    const profilePicture =
+      data.profilePicture &&
+      (await encodeProfileFieldFile(data.profilePicture));
+    dispatch(
+      signup({
+        email: data.email,
+        password: data.password,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        phoneNumber: data.phoneNumber,
+        location: {
+          address: data.address,
+          city: data.city,
+          state: data.state,
+          zipCode: data.zipCode,
+        },
+        photoRelease: data.photoRelease,
+        referrer: data.referrer,
+        profilePicture,
+        allergies: data.allergies,
+        medication: data.medications,
+        notes: data.otherNotes,
+        dateOfBirth: convertToYearMonthDateString(data.dateOfBirth),
+        pronouns: data.pronouns,
+        diagnosis: data.diagnosis,
+      }),
+    );
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Signup</title>
+        <meta
+          name="description"
+          content="Sign up for Lucy's Love Bus Events."
+        />
+      </Helmet>
+      <ContentContainer>
+        <SignupForm
+          onFinish={onFinish}
+          registeringAsParticipatingFamily={registeringAsParticipatingFamily}
+          tokens={tokens}
+        />
+      </ContentContainer>
+    </>
+    /**
+     * IDEA: create announcements form that takes initial values, reuse component for create and edit, delete is just a regular "delete" page with a confirmation checkbox
+     */
+  );
+};
+
+const mapStateToProps = (state: C4CState): SignupFormContainerProps => {
+  return {
+    tokens: state.authenticationState.tokens,
+  };
+};
+
+export default connect(mapStateToProps)(SignupFormContainer);
