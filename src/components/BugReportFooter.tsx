1335f646f39ab968e1e12cf7a52c1e325d793373
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-04-20 17:16:05 -0400

    looks good now

--- src/components/BugReportFooter.tsx
+++ src/components/BugReportFooter.tsx
@@ -2,19 +2,24 @@ import { BugOutlined } from '@ant-design/icons';
 import { Typography } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
+import { PRIMARY } from '../utils/colors';
 const { Text, Link } = Typography;
 const LittleBugIcon = styled(BugOutlined)`
   margin: 3px;
+  margin-right: 6px;
+  color: ${PRIMARY};
 `;
 const StickyFooter = styled.footer`
   position: fixed;
   left: 0;
   bottom: 0;
-  height: 25px;
+  height: 30px;
   width: 100%;
   display: flex;
   justify-content: center;
   background-color: rgba(255, 255, 255, 0.95);
+  border-top: 1px lightgray solid;
+  padding-top: 5px;
 `;
 
 const BugReportFooter: React.FC = () => {
@@ -22,9 +27,9 @@ const BugReportFooter: React.FC = () => {
     <StickyFooter>
       <LittleBugIcon />{' '}
       <Text>
-        Notice something off?{' '}
+        Notice an issue with the site?{' '}
         <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdaAuvzDfV4fb_SDbsfY3TSTwHKAyDa5dMgTnr1okE9COUTdA/viewform">
-          Submit a Bug Report!
+          Let us know!
         </Link>
       </Text>
     </StickyFooter>
