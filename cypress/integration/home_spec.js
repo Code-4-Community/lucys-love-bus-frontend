8b1b1ac44dcae278c5b8bac338ab569b7ff4e1ef
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-06-26 17:36:11 -0400

    update cypress tests

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,16 +1,20 @@
 describe('Home page', () => {
   it('should display no events text when no events exist', () => {
-    cy.visit('http://localhost:3000');
     // mock api, no results for events or announcements
-
     cy.intercept('/api/v1/events', {
-        events: [],
-        totalCount: 0,
+      events: [],
+      totalCount: 0,
     });
+
     cy.intercept('/api/v1/announcements', {
-        announcements: [],
-        totalCount: 0,
+      announcements: [],
+      totalCount: 0,
     });
+
+    // api calls must be stubbed before visiting anything since they are called immediately after visiting the page
+    
+    cy.visit('/');
+
     cy.contains('There are currently no events!');
     cy.contains('There are currently no announcements!');
   });
