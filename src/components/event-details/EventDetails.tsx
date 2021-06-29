93fde4db0f7ef6a9efa99f57d2ae364226044b53
Varun Thakkar <varunthakkar2013@gmail.com>
2021-06-28 21:09:10 -0400

    made card responsive

--- src/components/event-details/EventDetails.tsx
+++ src/components/event-details/EventDetails.tsx
@@ -11,11 +11,6 @@ import { AnnouncementCard } from '../AnnouncementCard';
 import EventRegistrationModal from '../modals/event-registration-modal/EventRegistrationModal';
 const { Title } = Typography;
 
-const TopRow = styled(Row)`
-  margin-bottom: 12px;
-  border: 1px solid #d9d9d9;
-`;
-
 const BottomRow = styled(Row)`
   display: flex;
   flex-direction: row;
@@ -26,6 +21,18 @@ const CardContent = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  border: 1px solid #d9d9d9;
+
+  @media screen and (max-width: 1450px) {
+    flex-direction: column;
+    width: min-content;
+    align-items: flex-start;
+  }
+
+  @media screen and (max-width: 900px) {
+    max-width: 85%;
+    min-width: 70%;
+  }
 `;
 
 const StyledTitle = styled(Title)`
@@ -43,6 +50,10 @@ const SeatsRemaining = styled.div`
 
 const Location = styled.div`
   margin-bottom: 120px;
+
+  @media screen and (max-width: 1450px) {
+    margin-bottom: 40px;
+  }
 `;
 
 const GreenButton = styled(Button)`
@@ -52,9 +63,7 @@ const GreenButton = styled(Button)`
 `;
 
 const Thumbnail = styled.img`
-  max-width: 100%;
-  height: 400px;
-  object-fit: cover;
+  display: flex;
 `;
 
 const EventsTag = styled(Tag)`
@@ -62,9 +71,43 @@ const EventsTag = styled(Tag)`
 `;
 
 const Info = styled.div`
+`;
+
+const CardWrapper = styled.div`
+  margin-bottom: 12px;
+`
+
+const ThumbnailWrapper = styled.div`
+  display: flex;
+  width: 55%;
+  height: 400px;
+  object-fit: cover;
+  justify-content: center;
+
+  @media screen and (max-width: 1450px) and (min-height: 900px) {
+    width: 100%;
+    min-height: max-content;
+    margin-bottom: 16px;
+  }
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+    min-height: 30%;
+    overflow: hidden;
+    margin-bottom: 16px;
+  }
+`
+
+const InfoWrapper = styled.div`
+  display: flex;
+  width: 45%;
   margin-left: 40px;
   max-height: 350px;
-`;
+
+  @media screen and (max-width: 1450px) {
+    width: 100%;
+  }
+`
 
 const AnnouncementBox = styled.div``;
 
@@ -119,12 +162,12 @@ const EventDetails: React.FC<EventDetailsProps> = ({
         <title>{title}</title>
         <meta name={title} content={details.description} />
       </Helmet>
-      <TopRow>
+      <CardWrapper>
         <CardContent>
-          <Col span={14}>
+          <ThumbnailWrapper>
             <Thumbnail src={thumbnail || DEFAULT_IMAGE}></Thumbnail>
-          </Col>
-          <Col span={10}>
+          </ThumbnailWrapper>
+          <InfoWrapper>
             <Info>
               <StyledTitle level={3}>{title}</StyledTitle>
               {ticketCount && (
@@ -143,9 +186,9 @@ const EventDetails: React.FC<EventDetailsProps> = ({
                 {hasRegistered ? 'Update Registration' : 'Register'}
               </GreenButton>
             </Info>
-          </Col>
+          </InfoWrapper>
         </CardContent>
-      </TopRow>
+      </CardWrapper>
       <BottomRow gutter={[24, 24]}>
         <Col span={14}>
           <Title level={5}>Description</Title>
